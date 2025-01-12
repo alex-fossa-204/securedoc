31eedee9c1c5585fb1d9463544d6237df0be249d
a.bakulin <shift.kite11@yandex.ru>
2025-01-12 21:31:34 +0300

    [master]: инициализация проекта

---
+++ src/main/java/com/getarrayz/securedoc/SecureDocApplication.java
@@ -0,0 +1,13 @@
+package com.getarrayz.securedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getarrayz/securedoc/SecureDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getarrayz.securedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
